b6b60249b3e476d3959dfa6daa82e69f1390c916
Bijoy chandro das <bijoychandrodas@Bijoys-MacBook-Air.local>
2023-01-28 05:33:21 -0800

    price filter

--- index.js
+++ index.js
@@ -40,6 +40,49 @@ async function run() {
       });
       res.send(airLines);
     });
+
+    app.get("/airLines/:name", async (req, res) => {
+      const name = req.params.name;
+      // const query = { Onwards: [{ OperatingCarrierName: name }] };
+      const result = await flightListCollection.find({}).toArray();
+      const airLine = result.filter(
+        (opt) => opt.Onwards[0].OperatingCarrierName === name
+      );
+
+      res.send(airLine);
+    });
+
+    // //Depart time
+    app.get("/departTimes/:time", async (req, res) => {
+      const time = req.params.time;
+      console.log(time);
+
+      const result = await flightListCollection.find({}).toArray();
+
+      const startingValue = time.split(" ")[0];
+      const endingValue = time.split(" ")[2];
+      const departFlights = result.filter(
+        (flight) =>
+          flight.Onwards[0].DepartureTime.slice(11, 16) >= startingValue &&
+          flight.Onwards[0].DepartureTime.slice(11, 16) <= endingValue
+      );
+
+      res.send(departFlights);
+    });
+
+    //price filter
+    app.get("/priceFilter/:price", async (req, res) => {
+      const price = req.params.price;
+      console.log(price);
+      const result = await flightListCollection.find({}).toArray();
+      const minPrice = price.split(",")[0];
+      const maxPrice = price.split(",")[1];
+      const priceFilterFlights = result.filter(
+        (flight) =>
+          flight.TotalPrice >= minPrice && flight.TotalPrice <= maxPrice
+      );
+      res.send(priceFilterFlights);
+    });
   } finally {
   }
 }
